b6711a9c083683a6a5526e5274d0f27d473a8110
DESKTOP-URFR41l <kristijan00b@gmail.com>
2022-01-05 03:03:50 +0100

    Kreirao sam formu za registraciju korisnika.

--- GameClient/DataLayer/UserRepository.cs
+++ GameClient/DataLayer/UserRepository.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shared.Models;
+using Shared.Interfaces;
 
 namespace DataLayer
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
 
         // Method for inserting User into database
---
+++ GameClient/PresentationLayer/RegistrationForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PanelHeader_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void LabelMinimize_MouseHover(object sender, EventArgs e)
+        {
+            LabelMinimize.ForeColor = Color.White;
+        }
+
+        private void LabelClose_MouseHover(object sender, EventArgs e)
+        {
+            LabelClose.ForeColor = Color.White;
+        }
+
+        private void LabelMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            LabelMinimize.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+        }
+
+        private void LabelClose_MouseLeave(object sender, EventArgs e)
+        {
+            LabelClose.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+        }
+
+        private void LabelMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void LabelClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void CheckEmptyFields()
+        {
+            if (textBoxName.Text == "")
+            {
+                labelErrorEmpty.Visible = true;
+                buttonErrorName.ForeColor = Color.Red;
+            }
+            if (textBoxLastname.Text == "")
+            {
+                labelErrorEmpty.Visible = true;
+                buttonErrorLastname.ForeColor = Color.Red;
+            }
+            if (textBoxEmail.Text == "")
+            {
+                labelErrorEmpty.Visible = true;
+                buttonErrorEmail.ForeColor = Color.Red;
+            }
+            if (textBoxPhone.Text == "")
+            {
+                labelErrorEmpty.Visible = true;
+                buttonErrorPhone.ForeColor = Color.Red;
+            }
+            if (textBoxPassword.Text == "")
+            {
+                labelErrorEmpty.Visible = true;
+                buttonErrorPassword.ForeColor = Color.Red;
+            }
+            if (textBoxConfirmPassword.Text == "")
+            {
+                labelErrorEmpty.Visible = true;
+                buttonErrorConfirmPassword.ForeColor = Color.Red;
+            }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            if (textBoxName.Text == "" || textBoxLastname.Text == "" || textBoxEmail.Text == "" || textBoxPhone.Text == "" || textBoxPassword.Text == "" || textBoxConfirmPassword.Text == "")
+            {
+                CheckEmptyFields();
+                return;
+            }
+        }
+
+        private void checkBoxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBoxShowPassword.Checked)
+            {
+                textBoxPassword.UseSystemPasswordChar = false;
+                textBoxConfirmPassword.UseSystemPasswordChar = false;
+            }
+            else {
+                textBoxPassword.UseSystemPasswordChar = true;
+                textBoxConfirmPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        public void ResetFormatStyles(List<Button> buttons, List<Label> labels)
+        {
+            foreach (var item in buttons)
+            {
+                item.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            }
+
+            foreach (var item in labels)
+            {
+                item.Visible = false;
+            }
+
+
+        }
+
+        private void textBox_MouseEnter(object sender, EventArgs e)
+        {
+            List<Button> buttons = new List<Button>() {buttonErrorName, buttonErrorLastname, buttonErrorEmail, buttonErrorPhone, buttonErrorPassword, buttonErrorConfirmPassword };
+            List<Label> labels = new List<Label>() { labelErrorFormat, labelErrorEmpty, labelFormatName, labelFormatLastaname, labelFormatEmail, labelFormatPhone, labelFormatPassword, labelFormatConfirmPassword };
+            ResetFormatStyles(buttons, labels);
+        }
+    }
+}
